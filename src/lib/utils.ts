5399bab05aa3d668150647b3ca229b631e92fd75
beyerenrico <enrico.beyer@synaigy.com>
2023-12-30 08:09:53 +0100

    feat(utils): add function to format pricing data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from 'tailwind-merge';
 export function cn (...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatPrice (
+  price: number | string,
+  options: {
+    locale?: 'en-US' | 'en-GB' | 'en-BD' | 'de-DE',
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT',
+    notation?: Intl.NumberFormatOptions['notation'],
+  } = {}
+) {
+  const {
+    locale = 'en-US',
+    currency = 'USD',
+    notation = 'compact'
+  } = options;
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numericPrice);
+}
